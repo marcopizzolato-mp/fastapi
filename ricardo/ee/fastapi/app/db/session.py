874e2a056847ec08c1828a19cea24fb2c5df2fa4
Marco P <marco.pizzolato@ricardo.com>
2024-07-07 21:50:21 +0100

    Database Session and get_db_session function

--- ricardo/ee/fastapi/app/db/session.py
+++ ricardo/ee/fastapi/app/db/session.py
@@ -1,35 +1,44 @@
 """Database Session."""
 
+from contextlib import contextmanager
 import os
 
+from dotenv import load_dotenv
 from loguru import logger
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.orm import sessionmaker
+
+# Load environment variables from .env file
+load_dotenv()
 
 db_host = os.getenv("DEFAULT_POSTGRES_HOST")
 db_user = os.getenv("DEFAULT_POSTGRES_USER")
 db_password = os.getenv("DEFAULT_POSTGRES_PASSWORD")
 db_name = os.getenv("DEFAULT_DB_NAME")
 
-connection_string = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}/{db_name}"
 
-engine = create_engine(
-    connection_string,
-    connect_args={"check_same_thread": False},
-    echo=True,
-)
-metadata = MetaData()
-database_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-# Dependency
+@contextmanager
 def get_db_session():
-    db_session = database_session()
+    connection_string = (
+        f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}/{db_name}"
+    )
+    print(connection_string)
+    engine = create_engine(
+        connection_string,
+        # connect_args={"check_same_thread": False}, # This is only for SQLLite
+        echo=True,
+    )
+    metadata = MetaData()
+    db_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    session = db_session()
     logger.info("Database session opened.")
+    print("Database session opened.")
     try:
-        yield db_session
+        yield session
     except Exception as e:
         logger.error(f"An error with the database occurred {e}.")
     finally:
-        logger.error("Database session closed.")
-        db_session.close()
+        logger.info("Database session closed.")
+        print("Database session closed.")
+        session.close()
