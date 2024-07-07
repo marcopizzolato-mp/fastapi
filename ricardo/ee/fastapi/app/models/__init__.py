d10d976eec95d35f33e25034dbf63d6ad36d4190
Marco P <marco.pizzolato@ricardo.com>
2024-07-07 14:09:56 +0100

    initiate ORM table objects

---
+++ ricardo/ee/fastapi/app/models/__init__.py
@@ -0,0 +1,7 @@
+from .conservation_effort import ConservationEfforts
+from .parks import Parks
+from .parks_facilities import ParkFacilities
+from .species import Species
+from .visitor import Visitors
+from .visits import Visits
+
