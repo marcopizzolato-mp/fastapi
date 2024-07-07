e0546daa6a5a779776124d088714a2b194789254
Marco P <marco.pizzolato@ricardo.com>
2024-07-07 14:28:55 +0100

    init ORM models

--- ricardo/ee/fastapi/app/models/__init__.py
+++ ricardo/ee/fastapi/app/models/__init__.py
@@ -1,7 +1,6 @@
-from .conservation_effort import ConservationEfforts
+from .conservation_efforts import ConservationEfforts
 from .parks import Parks
 from .parks_facilities import ParkFacilities
 from .species import Species
-from .visitor import Visitors
+from .visitors import Visitors
 from .visits import Visits
-
