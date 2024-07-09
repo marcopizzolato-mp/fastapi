2665525a0b6f432bdc24f186fec71210ff907fb4
Marco P <marco.pizzolato@ricardo.com>
2024-07-09 14:39:04 +0100

    pydantic schemas that map to the ORMs

---
+++ ricardo/ee/fastapi/app/schemas/schemas.py
@@ -0,0 +1,72 @@
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class Parks(BaseModel):
+    """Response model for Parks."""
+
+    park_id: int
+    name: str
+    location: str
+    area: float
+    established_date: date
+    description: str | None
+    type: str
+
+    # Fields from the Relationships
+    species_rel: list["Species"] | None
+    visitors_rel: list["Visitors"] | None
+    conservation_efforts_rel: list["ConservationEfforts"] | None
+    park_facilities_rel: list["ParkFacilities"] | None
+
+
+class Species(BaseModel):
+    """Response model for Species."""
+
+    species_id: int
+    common_name: str
+    scientific_name: str
+    status: str
+    habitat: str
+    description: str
+
+
+class Visitors(BaseModel):
+    """Response model for Visitors."""
+
+    visitor_id: int
+    name: str
+    email: str
+
+    # Fields from the Relationships
+    visits_rel: list["Visits"] | None
+
+
+class Visits(BaseModel):
+    visit_id: int
+    visitor_id: int
+    park_id: int
+    visit_start_date: date
+    visit_end_date: date
+
+
+class ConservationEfforts(BaseModel):
+    """Response model for ConservationEffort."""
+
+    effort_id: int
+    park_id: int
+    effort_name: str
+    start_date: date
+    end_date: date | None
+    description: str | None
+
+
+class ParkFacilities(BaseModel):
+    """Response model for ParkFacilities."""
+
+    facility_id: int
+    park_id: int
+    facility_type: str
+    name: str
+    description: str | None
