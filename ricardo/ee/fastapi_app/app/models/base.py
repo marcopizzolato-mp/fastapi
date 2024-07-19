e1487348373d4fbbe20788b7a3d11eef32bc4b3e
Marco P <marco.pizzolato@ricardo.com>
2024-07-19 16:04:52 +0100

    change docstring in base.py and comment out default schema assignment

--- ricardo/ee/fastapi_app/app/models/base.py
+++ ricardo/ee/fastapi_app/app/models/base.py
@@ -6,39 +6,43 @@ from ricardo.ee.fastapi_app.app.db.session import engine
 
 
 class Base(DeclarativeBase):
-    """Base class for SQLAlchemy models.
-
-    This base class automatically generates the `__tablename__` attribute
-    from the class name by converting it to lowercase. This eliminates the
-    need to manually specify the `__tablename__` attribute in each model class.
-
-    You can still specify the `__tablename__` attribute in each model
-    class for custom names.
-    """
+    """Base class for SQLAlchemy models."""
 
     @declared_attr
     def __tablename__(cls):  # noqa ANN204
         """Automatically generate the `__tablename__` attribute from the class name.
 
+        This  eliminates the need to manually specify the `__tablename__` attribute
+        in each model class.
+        You can still specify the `__tablename__` attribute in each model
+
         Args:
-            cls: The class for which the table name is being generated.
+                cls: The class for which the table name is being generated.
 
         Returns:
-            str: The table name derived from the class name, converted to lowercase.
+                str: The table name derived from the class name, converted to lowercase.
         """
         return cls.__name__.lower()
 
-    @declared_attr
-    def __table_args__(cls):  # noqa ANN204
-        """Set the schema for the table.
-
-        Args:
-            cls: The class for which the schema is being set.
-
-        Returns:
-            dict: A dictionary containing the schema name.
-        """
-        return {"schema": "public"}  # Specify the schema here
+    # @declared_attr
+    # def __table_args__(cls):  # ANN204
+    #     """Set the schema for the table.
+    #
+    #     If the database is SQLite it will ignore the schema.
+    #
+    #     Args:
+    #         cls: The class for which the schema is being set.
+    #
+    #     Returns:
+    #         dict: A dictionary containing the schema name.
+    #     """
+    #     print(engine.dialect.name)
+    #
+    #     if engine.dialect.name != "sqlite":
+    #         return {"schema": "public"}  # Specify schema for non-SQLite engines
+    #     else:
+    #         return {}  # No schema for SQLite
+    #     # return {"schema": "public"}  # Specify the schema here
 
 
 def init_db() -> None:
