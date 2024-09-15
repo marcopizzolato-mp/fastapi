e20ac365ed822b5900b1cbc7791c1ab119b48d22
Marco P <marcopizzolato-mp>
2024-09-15 20:45:23 +0100

    comment out tables initialization

--- fastapi_application/app/models/__init__.py
+++ fastapi_application/app/models/__init__.py
@@ -1,19 +1,19 @@
 """Init file for ORM Models."""
 
-from .conservation_efforts import ConservationEfforts
-from .parks import Parks
-from .parks_facilities import ParkFacilities
-from .parks_species import ParksSpecies
-from .parks_visits import ParkVisits
-from .species import Species
-from .visitors import Visitors
-
-__all__ = [
-    "Parks",
-    "ConservationEfforts",
-    "ParkFacilities",
-    "ParksSpecies",
-    "ParkVisits",
-    "Species",
-    "Visitors",
-]
+# from .conservation_efforts import ConservationEfforts
+# from .parks import Parks
+# from .parks_facilities import ParkFacilities
+# from .parks_species import ParksSpecies
+# from .parks_visits import ParkVisits
+# from .species import Species
+# from .visitors import Visitors
+#
+# __all__ = [
+#     "Parks",
+#     "ConservationEfforts",
+#     "ParkFacilities",
+#     "ParksSpecies",
+#     "ParkVisits",
+#     "Species",
+#     "Visitors",
+# ]
