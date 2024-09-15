22a64ce4cb1a2331e49a031f1c3d1672c530fa09
Marco P <marcopizzolato-mp>
2024-09-15 19:22:53 +0100

    review orm visitors

--- fastapi_application/app/models/visitors.py
+++ fastapi_application/app/models/visitors.py
@@ -1,15 +1,22 @@
 """ORM Model."""
 
-from fastapi_app.app.models.base import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from fastapi_application.app.models.base import Base
 
 
 class Visitors(Base):
     """ORM Model for Visitors table."""
 
+    __table_args__ = {"schema": "natural_parks_schema"}
+
     visitor_id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), nullable=False)
     email = Column(String(100), nullable=False)
+    marketing_consent = Column(Boolean, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    modified_at = Column(DateTime(timezone=True), onupdate=func.now())
 
     visits_rel = relationship("ParkVisits", back_populates="visitor_rel")
