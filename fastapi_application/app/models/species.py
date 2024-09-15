2ebc81bf3aecbcb2d3d1d0a475aa20e87f65ccca
Marco P <marcopizzolato-mp>
2024-09-15 19:21:59 +0100

    review orm species

--- fastapi_application/app/models/species.py
+++ fastapi_application/app/models/species.py
@@ -1,18 +1,24 @@
 """ORM Model."""
 
-from fastapi_app.app.models.base import Base
-from sqlalchemy import Column, Integer, String, Unicode
+from sqlalchemy import Column, DateTime, Integer, String, Unicode
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from fastapi_application.app.models.base import Base
 
 
 class Species(Base):
     """ORM Model for Species table."""
+
+    __table_args__ = {"schema": "natural_parks_schema"}
 
     species_id = Column(Integer, primary_key=True, index=True)
     common_name = Column(String(100), nullable=False)
     scientific_name = Column(String(100), nullable=False)
     status = Column(String(50), nullable=False)
     habitat = Column(String(100), nullable=False)
-    description = Column(Unicode(500), nullable=False)
+    description = Column(Unicode(500), nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    modified_at = Column(DateTime(timezone=True), onupdate=func.now())
 
     species_rel = relationship("ParksSpecies", back_populates="park_species_rel")
